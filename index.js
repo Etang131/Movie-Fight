6bea45b5ba130f31be80a11dd3a72e067869b889
Erik Tangvik <erikjt@outlook.com>
2019-12-19 17:36:36 -0600

    added a timeout function

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const fetchData = async () => {
+const fetchData = async searchTerm => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "dcf72138",
-      i: "tt0848228"
+      s: searchTerm
     }
   });
 
   console.log(response.data);
 };
 
-fetchData();
+const input = document.querySelector("input");
+
+const debounce = func => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    setTimeout(() => {
+      func.apply(null, args);
+    }, 1000);
+  };
+};
+
+let timeoutId;
+const onInput = event => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 1000);
+};
+
+input.addEventListener("input", onInput);
