42a257aea52abdd5b50c792f370b87d8aa5c5288
Erik Tangvik <erikjt@outlook.com>
2019-12-20 10:31:22 -0600

    Fixed search bar and added clicking movie selection

--- index.js
+++ index.js
@@ -31,17 +31,26 @@ const resultsWrapper = document.querySelector(".results");
 const onInput = async event => {
   const movies = await fetchData(event.target.value);
 
+  if (!movies.length) {
+    dropdown.classList.remove("is-active");
+    return;
+  }
+
   resultsWrapper.innerHTML = "";
   dropdown.classList.add("is-active");
   for (let movie of movies) {
     const option = document.createElement("a");
-    const imgSrc = movie.Poster === "N/A" ? "" : item.Poster;
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
 
     option.classList.add("dropdown-item");
     option.innerHTML = `
       <img src="${imgSrc}" />
       ${movie.Title}
     `;
+    option.addEventListener("click", () => {
+      dropdown.classList.remove("is-active");
+      input.value = movie.Title;
+    });
 
     resultsWrapper.appendChild(option);
   }
