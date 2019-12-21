4bbb347f3961fcefc6afc0c3e192ecd6549f3156
Erik Tangvik <erikjt@outlook.com>
2019-12-21 11:18:29 -0600

    synced up with new autocomplete and html files

--- index.js
+++ index.js
@@ -1,29 +1,48 @@
-const fetchData = async searchTerm => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      apikey: "dcf72138",
-      s: searchTerm
+const autoCompleteConfig = {
+  renderOption(movie) {
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    return `
+        <img src="${imgSrc}" />
+        ${movie.Title} (${movie.Year})
+      `;
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        apikey: "dcf72138",
+        s: searchTerm
+      }
+    });
+
+    if (response.data.Error) {
+      return [];
     }
-  });
 
-  if (response.data.Error) {
-    return [];
+    return response.data.Search;
   }
-
-  return response.data.Search;
 };
 
 createAutoComplete({
-  root: document.querySelector(".autocomplete")
+  ...autoCompleteConfig,
+  root: document.querySelector("#left-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#left-summary"));
+  }
 });
 createAutoComplete({
-  root: document.querySelector(".autocomplete-two")
-});
-createAutoComplete({
-  root: document.querySelector(".autocomplete-three")
+  ...autoCompleteConfig,
+  root: document.querySelector("#right-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#right-summary"));
+  }
 });
 
-const onMovieSelect = async movie => {
+const onMovieSelect = async (movie, summaryElement) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       apikey: "dcf72138",
@@ -31,45 +50,44 @@ const onMovieSelect = async movie => {
     }
   });
 
-  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
 };
 
 const movieTemplate = movieDetail => {
   return `
-<article class="media">
-<figure class="media-left">
-<p class="image">
-<img src="${movieDetail.Poster}" />
-</p>
-</figure>
-<div class="media-content">
-<div class="content">
-<h1>${movieDetail.Title}</h1>
-<h4>${movieDetail.Genre}</h4>
-<p>${movieDetail.Plot}</p>
-</div>
-</div>
-</article>
-<article class="notification is-primary">
-<p class="title">${movieDetail.Awards}</p>
-<p class="subtile">Awards</p>
-</article>
-<article class="notification is-primary">
-<p class="title">${movieDetail.BoxOffice}</p>
-<p class="subtile">Box Office</p>
-</article>
-<article class="notification is-primary">
-<p class="title">${movieDetail.Metascore}</p>
-<p class="subtile">Awards</p>
-</article>
-<article class="notification is-primary">
-<p class="title">${movieDetail.imdbRating}</p>
-<p class="subtile">IMDB Rating</p>
-</article>
-<article class="notification is-primary">
-<p class="title">${movieDetail.imdbVotes}</p>
-<p class="subtile">IMDB Votes</p>
-</article>
-
-`;
+      <article class="media">
+        <figure class="media-left">
+          <p class="image">
+            <img src="${movieDetail.Poster}" />
+          </p>
+        </figure>
+        <div class="media-content">
+          <div class="content">
+            <h1>${movieDetail.Title}</h1>
+            <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+          </div>
+        </div>
+      </article>
+      <article class="notification is-primary">
+        <p class="title">${movieDetail.Awards}</p>
+        <p class="subtitle">Awards</p>
+      </article>
+      <article class="notification is-primary">
+        <p class="title">${movieDetail.BoxOffice}</p>
+        <p class="subtitle">Box Office</p>
+      </article>
+      <article class="notification is-primary">
+        <p class="title">${movieDetail.Metascore}</p>
+        <p class="subtitle">Metascore</p>
+      </article>
+      <article class="notification is-primary">
+        <p class="title">${movieDetail.imdbRating}</p>
+        <p class="subtitle">IMDB Rating</p>
+      </article>
+      <article class="notification is-primary">
+        <p class="title">${movieDetail.imdbVotes}</p>
+        <p class="subtitle">IMDB Votes</p>
+      </article>
+    `;
 };
